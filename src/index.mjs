ceb23a6e46f74deb573678c7963ac6ee12a97679
ronanzindev <morellianogm@gmail.com>
2023-05-06 13:29:48 -0300

    Using jsx

--- src/index.mjs
+++ src/index.mjs
@@ -1,12 +1,20 @@
 import { h, render } from "https://unpkg.com/preact?module";
+import htm from "https://unpkg.com/htm?module";
 
-let i = 0;
+const html = htm.bind(h)
+
+function App(props) {
+    return html`
+        <div>
+        ${props.cpus.map((cpu) => {
+        return html`<div>${cpu.toFixed(2)}% usage</div> `
+    })}
+        </div>
+    `
+}
 setInterval(async () => {
     let response = await fetch('/api/cpus')
     if (response.status != 200) throw new Error(`Http error! status: ${response.status}`)
     let json = await response.json()
-    i = i + 1;
-    // document.body.textContent = JSON.stringify(json, null, 2)
-    const app = h('pre', null, JSON.stringify(json, null, 2))
-    render(app, document.body)
+    render(html`<${App} cpus=${json}></${App}>`, document.body)
 }, 1000)
